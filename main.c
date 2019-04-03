2ed2b7760484af59e8dca9c811aab7981b187740
raymond zhang <c3327979@uon.edu.au>
2019-04-03 08:54:24 +0000

    tried to assign a word onto secret, should convert the letters into ASCII, and shift those values by 13 for an easy decryption, edited it last second without testing see if it works

--- main.c
+++ main.c
@@ -1,7 +1,43 @@
+/* Main function of the C program. */
+
 #include <stdio.h>
+#include <stdlib.h>
+
+
 
 int main()
 {
-printf("hello world");
-return 0;
+    char secret[122], eh;
+    int i;
+    int shift = 13;
+        printf( "enter a secret:" );
+    
+     gets(secret);
+   
+    
+    for(i = 0; secret[i] != '\0'; i = i + 1)
+{
+    eh = secret[i];
+    
+    if (eh >= 65 && eh <= 90) 
+    {
+        eh = eh + shift;
+            
+            secret[i] = eh;
+    }
+        else if(eh >= 97 && eh <= 122)
+        {
+            eh = eh + shift;
+                if(eh > 122)
+                {
+                    eh = eh - 122 + 98 - 32;
+                }
+                secret[i] = eh;
+        }
+    
+    
 }
+        printf("hidden secret: %s", secret);
+        return 0;
+}
+
