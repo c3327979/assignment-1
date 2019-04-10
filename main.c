b22024fb89fdddca78de81984c3159033c0cae8f
c3327979 <49225111+c3327979@users.noreply.github.com>
2019-04-10 20:00:48 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-/* Main function of the C program. */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,37 +6,44 @@
 
 int main()
 {
-    char secret[122], eh;
+    char secret[122], eh;                           //this is the range of numbers what you wanna type can be (since ASCII letters range from 65 - 122)
     int i;
-    int shift = 13;
-        printf( "enter a secret:" );
+    int shift = 1;                                  // the number of letters its gonna change by so a moves one space so t'd be b and c = d etc
+        printf( "enter a secret:" );                // just a user prompt does nothing but make it easier for a suer to get
     
-     gets(secret);
+     gets(secret);                                  // this collects the user input and stores it into 'secret'
    
     
     for(i = 0; secret[i] != '\0'; i = i + 1)
 {
-    eh = secret[i];
+    eh = secret[i];                                 // this is jsut to separate 'secret' from any equations to make the maths easier, i just chose 'eh' for some reason. may be unnecassary but im not gonna test that theory
     
-    if (eh >= 65 && eh <= 90) 
+    if (eh >= 'A' && eh <= 'Z')                     // this is to say if the input is a capital letter execute the equation below
     {
-        eh = eh + shift;
-            
-            secret[i] = eh;
-    }
-        else if(eh >= 97 && eh <= 122)
+        if(eh >= 'Z')
         {
-            eh = eh + shift;
-                if(eh > 122)
-                {
-                    eh = eh - 122 + 98 - 32;
-                }
+            eh = 64;                                // this is in case the letter 'Z' is used to set the value as 64 so when the shift occurs it becomes 65 or 'A'
+        }
+        eh = eh + shift - 65 + 'A'  ;           // this is theactual letter shift where all the numbers are converted to range from 0-25 with the minus '64', the shift moves the value up one, then adding 'A' to return the value to ASCII letters
+        
+            secret[i] = eh;                         // this returns the value of 'eh' to secret for printing
+    }
+        else if(eh >= 'a' && eh <= 'z')             // this heres for lower case letters, otherwise everything else is the same as above
+        {
+            
+               if(eh >= 'z')
+               {
+                   eh = 96;
+               }
+               eh = eh + shift - 97 + 'A';
                 secret[i] = eh;
         }
     
     
 }
-        printf("hidden secret: %s", secret);
+        printf("hidden secret: %s", secret);        //this just prints the encoded bit
         return 0;
 }
+
+
 
